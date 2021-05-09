3e2391354d7872f96159584918a36058f89bf9a1
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2021-05-09 15:25:28 +0100

    Add files via upload

---
+++ total.py
@@ -0,0 +1,9 @@
+import time
+b = int(input("ENTER BENCH IN KG FUCK LBS "))
+s = int(input("ENTER SQUAT IN KG FUCK LBS "))
+d = int(input("ENTER DEADLIFT IN KG FUCK LBS "))
+total = b + s + d
+totallb = total * 2.205
+print("ur total is "+str(total)+" in KG")
+print("ur total is "+str(totallb)+" in LB")
+time.sleep(2000)
